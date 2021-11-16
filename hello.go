aaaf4896dee38c5cc5227a215bb651f70e6f635f
Jonathan Zhao <jinqiangzhao2017@gmail.com>
2021-11-16 15:17:33 -0800

    package "morestrings" added.

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/smstong/hello-go/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("Hello, Go"))
+	fmt.Println(cmp.Diff("Hello world", "Hello go"))
 }
